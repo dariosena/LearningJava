98c9fd01594bb55becf689b04e9bd53da4af761c
Emanuel Sena <dariosena@gmail.com>
2019-10-05 22:54:41 -0300

    Review Java.

---
+++ samples/MeuPrograma.java
@@ -0,0 +1,7 @@
+
+class MeuProgram {
+    public static void main(String[] args) {
+        System.out.println("Minha primeira aplicação Java");
+        System.out.println("Emanuel D R Sena\nJava Learning!");
+    }
+}
