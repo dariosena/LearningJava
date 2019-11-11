ec2ec67919f695d4df475fbe541872c9752b139c
Emanuel Sena <dariosena@gmail.com>
2019-11-11 16:10:09 -0300

    Java How To Program

---
+++ samples/howtoprogram/Welcome2.java
@@ -0,0 +1,12 @@
+package howtoprogram;
+
+// Fig. 2.3: Welcome2.java
+// Printing a line of text with multiple statements.
+
+public class Welcome2 {
+    // main method begins execution of Java Application
+    public static void main(String[] args) {
+        System.out.print("Welcome to ");
+        System.out.println("Java Programming!");
+    } // end of method main
+} // end of class Welcome2
---
+++ samples/howtoprogram/Welcome4.java
@@ -0,0 +1,11 @@
+package howtoprogram;
+
+// Fig. 2.6: Welcome4.java
+// Display multiple lines with a method System.out.printf.
+
+public class Welcome4 {
+    // main method begins execution of Java Application
+    public static void main(String[] args) {
+        System.out.printf("%s%n%s%n", "Welcome to", "Java Programming!");
+    } // end of method main
+} // end of class Welcome4
